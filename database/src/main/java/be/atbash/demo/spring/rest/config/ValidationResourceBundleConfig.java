837a6c931010916c40eb5be6dd48ae3111c2607d
Rudy De Busscher <rudy.debusscher@qarad.com>
2026-01-03 11:44:45 +0100

    update file headers

--- database/src/main/java/be/atbash/demo/spring/rest/config/ValidationResourceBundleConfig.java
+++ database/src/main/java/be/atbash/demo/spring/rest/config/ValidationResourceBundleConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024-2025 Rudy De Busscher (https://www.atbash.be)
+ * Copyright 2024-2026 Rudy De Busscher (https://www.atbash.be)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
